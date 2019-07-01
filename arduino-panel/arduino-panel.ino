a5421c21b8176386fd0b5b333bfc1d76d424977e
Kevin Peña <kevin.pena.prog@gmail.com>
2019-07-01 16:43:33 -0500

    Add class to keep a moving average

--- arduino-panel/arduino-panel.ino
+++ arduino-panel/arduino-panel.ino
@@ -1,3 +1,27 @@
+template<typename T, int SIZE, typename A>
+class Average {
+  T data[SIZE];
+  T* current;
+public:
+  Average() : current(data) {}
+  
+  void add_val(T val)
+  {
+    *current = val;
+    ++current;
+    if (current >= (data + SIZE)) current = data;
+  }
+
+  T get_val()
+  {
+    A sum = 0;
+    for (auto val : data) {
+      sum += val;
+    }
+    return sum / SIZE;
+  }
+};
+
 void setup() {
   
 }
