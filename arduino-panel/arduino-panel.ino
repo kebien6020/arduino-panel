c47b3fb4a73da8d8cafe7ffd0a1b7d5b4149a2d1
Kevin Peña <kevin.pena.prog@gmail.com>
2019-07-01 16:47:08 -0500

    Add constants for averages

--- arduino-panel/arduino-panel.ino
+++ arduino-panel/arduino-panel.ino
@@ -7,6 +7,11 @@ constexpr int PIN_BATERIA = A0;
 constexpr int PIN_SALIDA = 5;
 constexpr int PIN_DISPLAY_SDA = 6;
 constexpr int PIN_DISPLAY_SCL = 7;
+// Constantes de promedios
+constexpr int MUESTRAS_PROMEDIO_BATERIA = 100;
+constexpr int TIEMPO_ENTRE_MUESTRAS_BATERIA = 10;
+constexpr int MUESTRAS_PROMEDIO_SALIDA = 100;
+
 template<typename T, int SIZE, typename A>
 class Average {
   T data[SIZE];
